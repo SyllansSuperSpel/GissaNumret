0867d50cc7b79d88adad6f3c5d429fe6ef919757
SyllansSuperSpel <36716088+SyllansSuperSpel@users.noreply.github.com>
2018-02-22 00:09:47 +0100

    Create spel.py

---
+++ spel.py
@@ -0,0 +1,25 @@
+"""
+Gissa numret
+"""
+
+# Nån voodoo grej
+import random
+numret = random.randint(1, 100)
+
+# Mitt spel
+while True:
+    gissning = int(input('Gissa ett nummer: '))
+
+    if gissning == numret:
+        print("Yay!")
+        break
+
+    elif gissning > numret:
+        print("Lägre")
+
+    else: 
+        print("Högre")
+
+    
+
+print("Nu var det slutspelat!")
